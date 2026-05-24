93f63d0183ee7a48938773726f83a685a5129126
Mohammed Sharook <msharuk589@gmail.com>
2026-05-24 13:40:39 +0530

    Added user access check

--- src/generation/main.py
+++ src/generation/main.py
@@ -45,6 +45,7 @@ def run_query(query, pc, pc_index,
         reranker_model = reranker_model[1]
     )
 
+    cache_hit = False
     if cache_hit:
       return (cache_hit, 
               cache_docs,
@@ -64,49 +65,49 @@ def run_query(query, pc, pc_index,
       )
 
       merged_docs = merge_ranked_chunks(retrieved_docs.data)
-      sources = [doc['id'] for doc in merged_docs]
+      return merged_docs
+    #   sources = [doc['id'] for doc in merged_docs]
 
-      answer, total_tokens, finish_reason = generate_response(query, merged_docs)
+    #   answer, total_tokens, finish_reason = generate_response(query, merged_docs)
 
-    #   store_in_cache(pc_index, query, answer, 
-    #         query_dense_embedding, 
-    #         query_sparse_embedding,
-    #         sources)
+    # #   store_in_cache(pc_index, query, answer, 
+    # #         query_dense_embedding, 
+    # #         query_sparse_embedding,
+    # #         sources)
 
-      return False, merged_docs, answer, total_tokens, finish_reason
+    #   return False, merged_docs, answer, total_tokens, finish_reason
 
 if __name__ == "__main__":
     (pc, pc_index, dense_tokenizer, dense_model, 
      sparse_tokenizer, sparse_model, sparse_input_names, 
      reranker) = initialize_search_pipeline()
 
-    # query = "What are the specific gate thresholds used to automatically decide whether a compressed model variant is allowed, canaried, or blocked, including the limits for chat similarity drop, code pass rate change, retrieval embedding quality, and acceptable latency and cost changes?"
-    # query = "In the draft spec about extending a routing policy engine for automated regional failover, what is the proposed priority order for evaluating different failure signals when deciding whether to shift traffic or fail over?"
-    # query = "When is the 60 to 90 minute technical deep dive scheduled with the healthcare client about running model serving inside their own isolated network, and what is the time window in Pacific time?"
-    # query = "In the notes about keeping long, stop-and-go chat sessions cheap without replaying the whole history, what storage setup and time-to-live were proposed for keeping the compact per-session state for recent sessions versus longer retention?"
-
-    # query = "What are the default size limits for file uploads and total request size for the new multipart upload support on the OpenAI-compatible API endpoints?"
+    
     query = "What is the name of the new metric added so SRE can track when server-side streaming sessions get finalized due to hitting the time limit?"
 
-    start_time = time.perf_counter()
-    (is_cache_hit, retrieved_docs, 
-    answer, total_tokens, 
-    finish_reason) = run_query(query, pc, pc_index, 
+    user = 'Liam Chen'
+
+    # start_time = time.perf_counter()
+    # (is_cache_hit, retrieved_docs, 
+    # answer, total_tokens, 
+    # finish_reason) = run_query(query, pc, pc_index, 
+    #                             dense_tokenizer, dense_model, 
+    #                             sparse_tokenizer, sparse_model, 
+    #                             sparse_input_names, reranker)
+
+    # end_time = time.perf_counter()
+
+    # print(f'Time taken: {end_time - start_time:.2f} seconds')
+    # print(f'Is Cache hit : {is_cache_hit}')
+    # print('Total tokens used:', total_tokens)
+    # print('Finish reason:', finish_reason)
+
+    # print("\n=== Answer ===")
+    # print(answer)
+
+    retrieved_docs = run_query(query, pc, pc_index, 
                                 dense_tokenizer, dense_model, 
                                 sparse_tokenizer, sparse_model, 
                                 sparse_input_names, reranker)
 
-    # retrieved_docs = run_query(query[1], pc, pc_index, 
-    #           dense_tokenizer, dense_model, 
-    #           sparse_tokenizer, sparse_model, 
-    #           sparse_input_names, reranker)
 
-    end_time = time.perf_counter()
-
-    print(f'Time taken: {end_time - start_time:.2f} seconds')
-    print(f'Is Cache hit : {is_cache_hit}')
-    print('Total tokens used:', total_tokens)
-    print('Finish reason:', finish_reason)
-
-    print("\n=== Answer ===")
-    print(answer)
--- src/retrieval/vector_store.py
+++ src/retrieval/vector_store.py
@@ -1,55 +1,39 @@
-from tqdm import tqdm
-from pinecone import Pinecone, ServerlessSpec
-
 from src.utils.logger import logger
-from src.retrieval.config import (PINECONE_API_KEY, PINECONE_INDEX_NAME, 
-                                  SOURCES, TOP_K_PER_SOURCE, PINECONE_NAMESPACE)
-
-
-def get_pinecone_index():
-    try:
-        logger.info("Connecting to Pinecone...")
-        pc = Pinecone(api_key=PINECONE_API_KEY)
-        return pc, pc.Index(PINECONE_INDEX_NAME) 
-    
-    except Exception as error:
-        logger.error(f'Error occured')
-        logger.error(error)
-
-        return None, None
+from src.retrieval.config import (SOURCES, TOP_K_PER_SOURCE, PINECONE_NAMESPACE)
 
 
 def query_all_sources(index, dense_vector, 
-                      sparse_vector = None, 
+                      sparse_vector,
+                      current_user_name, 
                       top_k = TOP_K_PER_SOURCE) -> list[dict]:
     futures = []
-    for source in SOURCES:        
-        future = index.query(
-            top_k=top_k,
-            vector=dense_vector,
-            sparse_vector=sparse_vector if sparse_vector and sparse_vector["indices"] else None,
-            include_values=False,
-            include_metadata=True,
-            filter={"source": {"$eq": source}},
-            async_req=True,
-            namespace=PINECONE_NAMESPACE
-        )
-        futures.append(future)
+    # for source in SOURCES:        
+    future = index.query(
+        top_k=top_k,
+        vector=dense_vector,
+        sparse_vector=sparse_vector,
+        include_values=False,
+        include_metadata=True,
+        filter={"user_access": {"$in": [current_user_name]}},
+        async_req=True,
+        namespace=PINECONE_NAMESPACE
+    )
+    futures.append(future)
 
     documents = []
-    for future in futures:
-        response = future.get()
-        matches = response.to_dict()["matches"]
-        docs = [
-            {
-                "id": m["id"],
-                "score": m["score"],
-                "chunk_text": m["metadata"]["text"],
-                'doc_id': m['metadata']['dataset_doc_uuid']
-            }
-            for m in matches
-        ]
-        documents.extend(docs)
+    # for future in futures:
+    response = future.get()
+    matches = response.to_dict()["matches"]
+    documents = [
+        {
+            "id": m["id"],
+            "score": m["score"],
+            "chunk_text": m["metadata"]["text"],
+            'doc_id': m['metadata']['dataset_doc_uuid']
+        }
+        for m in matches
+    ]
+    # documents.extend(docs)
 
     return sorted(documents, key=lambda x: x["score"], reverse=True)
 
